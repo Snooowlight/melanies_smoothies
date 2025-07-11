612982f44e0785fd08b899cddcfb3d831b4beaa7
Snooowlight <lightess22@gmail.com>
2025-07-11 09:34:05 +0200

    Update streamlit_app.py
    add subheader to choosen fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,7 @@ if ingredients_list :
     
     for i in ingredients_list : 
         ingredients_string += i + ' '
+        st.subheader(i + ' Nutrition information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+i)
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True )
         
