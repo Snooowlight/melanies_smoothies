03dcaa6d359f420e395bd4c9049b37d8acb7aae1
Snooowlight <lightess22@gmail.com>
2025-07-11 10:27:00 +0200

    Update streamlit_app.py
    add st.stop to debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,7 @@ if ingredients_list :
         ingredients_string += i + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.stop()
       
         st.subheader(i + ' Nutrition information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+i)
